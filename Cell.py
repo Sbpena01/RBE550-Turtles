016a749e78978effeb795bf8d461eac7bf775ca6
Scott Pena <scottpena@Scotts-MacBook-Pro.local>
2024-08-30 14:19:01 -0400

    Deleted cell.py and my pseudocode text file

--- Cell.py
+++
@@ -1,6 +0,0 @@
-class Cell:
-    def __init__(self, position: tuple) -> None:
-        self.position = position
-        
-    def changePosition(self, new_position) -> None:
-        self.position = new_position
